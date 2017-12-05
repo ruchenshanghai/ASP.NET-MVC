47ce34548731b482f0023fbb5f933dd1566c4f6e
濡尘赏海 <996956441@qq.com>
2017-12-05 16:01:23 +0800

    initial study

---
+++ Tutorial/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial.Controllers
+{
+    public class TestController : Controller
+    {
+        //// GET: Test
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: /Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Test/Second
+        public String Second(int id)
+        {
+            return "This is the Second action method.... id: " + id;
+        }
+        
+        // GET: /Test/Third
+        public String Third(String name, int id)
+        {
+            return "This is the Third action method.... name: " + name + " id: " + id;
+        }
+    }
+}
